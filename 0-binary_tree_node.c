f8929b11c5819277fd0a68b8fb4a8ed1112ffe34
Youssef-Saad-99 <133709470+Youssef-Saad-99@users.noreply.github.com>
2024-03-27 00:08:26 +0200

    Update 0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -11,19 +11,14 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-binary_tree_t *new_node = (binary_tree_t *) malloc(sizeof(binary_tree_t));
-if (new_node == NULL)
+binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+if (new_node =!NULL)
 {
-return NULL;
-}
-else
-{
-binary_tree_t *new_node = (binary_tree_t *) malloc(sizeof(binary_tree_t));
 new_node -> parent = parent;
 new_node -> n = value;
 new_node -> right = NULL;
 new_node -> left = NULL;
+}
 
 return (new_node);
-}
 }
