ab630c8def09dea91c91c7a9fab93b312e2b4e9a
Youssef-Saad-99 <133709470+Youssef-Saad-99@users.noreply.github.com>
2024-03-27 23:38:01 +0200

    Update 9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -13,11 +13,11 @@ size_t left_height, right_height;
 if (tree == NULL)
 return (0);
 
-left_height = binary_tree_height(tree->left); // Height of left subtree
-right_height = binary_tree_height(tree->right); // Height of right subtree
+left_height = binary_tree_height(tree->left);
+right_height = binary_tree_height(tree->right);
 
 if (left_height > right_height)
-return (left_height + 1); // Add 1 to account for the current node
+return (left_height + 1);
 else
-return (right_height + 1); // Add 1 to account for the current node
+return (right_height + 1);
 }
