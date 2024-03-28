7b9394e731a9113dd83440f45f0584666a3e46e3
Youssef-Saad-99 <133709470+Youssef-Saad-99@users.noreply.github.com>
2024-03-28 21:39:53 +0200

    Update binary_trees.h

--- binary_trees.h
+++ binary_trees.h
@@ -1,10 +1,11 @@
 #ifndef _BINARY_TREES_H_
 #define _BINARY_TREES_H_
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <limits.h>
-
+#include <stddef.h>
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -21,16 +22,20 @@ struct binary_tree_s *left;
 struct binary_tree_s *right;
 };
 
+/*  linked list for advanced 101 the levelorder */
 /**
- * struct levelorder_queue_s - Level order traversal queue.
- * @node: A node of a binary tree.
- * @next: The next node to traverse to in the binary tree.
+ * struct link_s - structure for advanced tasks
+ *
+ * @n: depth of node specified
+ * @node: node of tree to store
+ * @next: next node of the linked list
  */
-typedef struct levelorder_queue_s
+typedef struct link_s
 {
-	binary_tree_t *node;
-	struct levelorder_queue_s *next;
-} levelorder_queue_t;
+	size_t n;
+	struct binary_tree_s const *node;
+	struct link_s *next;
+} link_t;
 
 typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s bst_t;
@@ -58,7 +63,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 binary_tree_t *binary_trees_ancestor(
-	const binary_tree_t *first, const binary_tree_t *second);
+const binary_tree_t *first, const binary_tree_t *second);
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 int binary_tree_is_complete(const binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
@@ -78,6 +83,5 @@ heap_t *heap_insert(heap_t **root, int value);
 heap_t *array_to_heap(int *array, size_t size);
 int heap_extract(heap_t **root);
 int *heap_to_sorted_array(heap_t *heap, size_t *size);
-
 
 #endif
